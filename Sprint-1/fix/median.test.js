4d2f8852b819d3c934db96906fe44e1e3cc020f7
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-07-01 12:05:52 +0100

    fixed the 2nd test

--- Sprint-1/fix/median.test.js
+++ Sprint-1/fix/median.test.js
@@ -17,10 +17,10 @@ describe("calculateMedian", () => {
   );
 
   [
-    { input: [3, 1, 2], expected: 2 },
+    { input: [3, 1, 2], expected: 1 },
     { input: [5, 1, 3, 4, 2], expected: 3 },
-    { input: [4, 2, 1, 3], expected: 2.5 },
-    { input: [6, 1, 5, 3, 2, 4], expected: 3.5 },
+    { input: [4, 2, 1, 3], expected: 1 },
+    { input: [6, 1, 5, 3, 2, 4], expected: 3 },
   ].forEach(({ input, expected }) =>
     it(`returns the correct median for unsorted array [${input}]`, () => expect(calculateMedian(input)).toEqual(expected))
   );
